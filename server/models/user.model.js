bf709f6c4efaa14f22f2315ea6df2bc77ee0963c
behzad <behzad.b.i.g@gmail.com>
2022-06-08 16:21:42 +0430

    user.model compeleted

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -11,8 +11,51 @@ const validator = require('validator');
 require('dotenv').config();
 
 const userSchema = mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validate(value){
+            if (!validator.isEmail(value)) {
+                throw new Error("Invalid Email");
+            }
+        }
+    },
+    password:{
+        type: String,
+        required:true,
+        trim: true,
+    },
+    role:{
+        type:String,
+        enum: ['user','behzades'],
+        default:'user',
+    },
+    firstname: {
+        type: String,
+        maxLength: 100,
+        trim: true,
+    },
+    lastname:{
+        type: String,
+        maxLength: 100,
+        trim: true,    
+    },
+    age:{
+        type: Number,
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+},{
+    //timestamps: true,
+    //collection: "Players"  //changes the name of the schema
+});
 
-})
+
 
 
 const User = mongoose.model('User', userSchema);
